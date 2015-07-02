9763e5407309cbb2e843b801e0ed7329e5d53e0b
Shane Coll <shanewilliamcoll@gmail.com>
2015-07-01 20:47:51 -0400

    - Wrote header

---
+++ edd-fes-ddg.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Easy Digital Downloads Dummy Data Generator
+ * Description: Generates dummy data for stores powered by the Easy Digital Downloads plugin
+ * Version:     0.1
+ * Author:      Shane W. Coll
+ * Author URI:  shanecoll.com
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
